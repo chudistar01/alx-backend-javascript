a9e822392eb24058c2266eccfd9dca09894cf1a8
chudistar01 <christianigwebuike31@gmail.com>
2024-09-16 12:16:54 +0000

    javascripts

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,6 +1,6 @@
 export default function taskBlock(trueOrFalse) {
-    const task = false; // eslint-disable-line no-unused-vars
-    const task2 = true; // eslint-disable-line no-unused-vars
+  const task = false; // eslint-disable-line no-unused-vars
+  const task2 = true; // eslint-disable-line no-unused-vars
 
   if (trueOrFalse) {
     const task = true; // eslint-disable-line no-unused-vars
