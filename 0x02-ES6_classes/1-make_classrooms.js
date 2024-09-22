557d2341fc70979057136793e34b3647a22be679
chudistar01 <christianigwebuike31@gmail.com>
2024-09-22 20:04:38 +0000

    javascripts

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,13 @@
 import ClassRoom from './0-classroom';
 
 function initializeRooms() {
+  const obj1 = new ClassRoom(19);
+  const obj2 = new ClassRoom(20);
+  const obj3 = new ClassRoom(34);
   return [
-    new ClassRoom(19),
-    new ClassRoom(19),
-    new ClassRoom(19),
+    obj1,
+    obj2,
+    obj3,
   ];
 }
 export default initializeRooms;
