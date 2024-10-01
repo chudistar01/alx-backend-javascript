f26bef09bd6af9620d80d75ae8d1c609b10c671d
chudistar01 <christianigwebuike31@gmail.com>
2024-10-01 19:17:12 +0000

    javascripts

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * Displays a message to the standard output
+ * @param (displayed message)
+ * @author Igwebuike Christian
+ */
+function displayMessage(message) {
+    console.log(message)
+}
+
+module.exports = displayMessage;
+ 
