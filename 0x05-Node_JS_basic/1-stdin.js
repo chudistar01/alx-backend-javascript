7ce7a1e0ad8d03105f284a6672f0ba802022a112
chudistar01 <christianigwebuike31@gmail.com>
2024-10-01 22:19:54 +0000

    javascripts

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdin.setEncoding('utf-8');
 
-process.stdout.write('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data.trim()}`);
