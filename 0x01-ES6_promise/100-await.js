813b75363df4e1e1eec9f0c5c126c9013c3ea315
chudistar01 <christianigwebuike31@gmail.com>
2024-11-05 05:04:51 -0800

    javascripts

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,15 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default asyn function asyncUploadUser() {
-    let res = {};
+export default async function asyncUploadUser() {
+  let res = {};
 
-
-    try {
-	const photo = await uploadPhoto();
-	const user = await createUser();
-	res = { photo, user };
-    } catch (err) {
-	res = { photo: null, user: null };
-    }
-    result res;
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    res = { photo, user };
+  } catch (err) {
+    res = { photo: null, user: null };
+  }
+  return res;
 }
